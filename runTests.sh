ddcfb77142ee1f7fa2524a307761892efa0d7f21
ImGugz <gustavoaaguiar@tecnico.ulisboa.pt>
2020-11-17 01:53:57 +0000

    Numeric Validation Script

--- runTests.sh
+++ runTests.sh
@@ -8,6 +8,7 @@ maxthreads=$3
 # argument validation
 [ ! -d "$inputdir" ] && { echo "Input directory $inputdir doesn't exist. Please try again."; exit 1; }
 [ ! -d "$outputdir" ] && { echo "Output directory $outputdir doesn't exist. Please try again."; exit 1; }
+if [[ -n ${maxthreads//[0-9]/} ]]; then echo "Max number of threads must be numeric. Please try again."; exit 1; fi
 [ ! $maxthreads -gt 0 ] && { echo "Max number of threads must be greater than 0. Please try again."; exit 1; }
 
 for file in "$inputdir"/*; 
